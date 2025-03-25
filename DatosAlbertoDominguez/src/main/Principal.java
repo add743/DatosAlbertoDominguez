16e3f6c561f684be871bc4258ceb1e64d2fb3c81
Alberto Dominguez Domingo <add743@educa.madrid.org>
2025-03-25 20:54:52 +0100

    Versión con edad

--- DatosAlbertoDominguez/src/main/Principal.java
+++ DatosAlbertoDominguez/src/main/Principal.java
@@ -3,7 +3,9 @@ package main;
 public class Principal {
 
 	public static void main(String[] args) {
+		int edad = 38;
 		System.out.println("Alberto Domínguez Domingo");
 		System.out.println("Segunda versión");
+		System.out.println("Edad del alumno: " + edad);
 	}
 }
